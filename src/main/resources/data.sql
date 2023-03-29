7d091bfa6475ce7fc62bc601f1c36e260b0ea54d
Maciej Glownia <maciej.glownia@infor.com>
2023-03-29 20:56:16 +0200

    Add first records using data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into course(id, name) values(1,'5 basics commands to practice everyday.');
+insert into course(id, name) values(2,'How to build relationship with your dog?');
+insert into course(id, name) values(3,'Everything about feeding.');
