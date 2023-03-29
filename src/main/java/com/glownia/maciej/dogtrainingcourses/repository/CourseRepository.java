b420970c80f95c25a0fac53f5f169c437ee1d1bf
Maciej Glownia <maciej.glownia@infor.com>
2023-03-29 21:06:42 +0200

    Add CourseRepository class

---
+++ src/main/java/com/glownia/maciej/dogtrainingcourses/repository/CourseRepository.java
@@ -0,0 +1,13 @@
+package com.glownia.maciej.dogtrainingcourses.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class CourseRepository {
+
+    @Autowired
+    EntityManager entityManager;
+}
