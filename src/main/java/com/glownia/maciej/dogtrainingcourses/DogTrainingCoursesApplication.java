a50b1872e02e3086d2cb1cafd02a8e8b0f9d32dd
Maciej Glownia <maciej.glownia@infor.com>
2023-03-29 19:02:03 +0200

    Initial commit

---
+++ src/main/java/com/glownia/maciej/dogtrainingcourses/DogTrainingCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.glownia.maciej.dogtrainingcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogTrainingCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogTrainingCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glownia/maciej/dogtrainingcourses/DogTrainingCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glownia.maciej.dogtrainingcourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogTrainingCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
